ddd5dc70af9d4f096ff3c5a32dffc71f79723d01
Sihle Socishe <sihlesocishe@gmail.com>
2020-10-11 17:44:26 +0200

    Updates

---
+++ precache-manifest.017d47182de3c710890ba065a6cef381.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de011c63220850fce03de328df2301d5",
+    "url": "/redux-counter/index.html"
+  },
+  {
+    "revision": "861cce980c8be73447c3",
+    "url": "/redux-counter/static/css/main.79c65ae2.chunk.css"
+  },
+  {
+    "revision": "97dc7eb7950b5a00bbc4",
+    "url": "/redux-counter/static/js/2.247f49ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-counter/static/js/2.247f49ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "861cce980c8be73447c3",
+    "url": "/redux-counter/static/js/main.4066af94.chunk.js"
+  },
+  {
+    "revision": "6b20e1a0169180f3c003",
+    "url": "/redux-counter/static/js/runtime-main.8c20eaa4.js"
+  }
+]);
